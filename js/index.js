c00bf8054d4ddabe3ef9e8fd6089146ceb7b1176
Douglas Wangome <ishdagnesh@gmail.com>
2022-12-06 00:28:08 +0300

    eleventh commit

--- js/index.js
+++ js/index.js
@@ -1,4 +1,17 @@
 // Header
+let staggered = false;
+const menu_icon = document.getElementById('menu-icon').onclick = () => {
+  staggered = !staggered;
+  if (!staggered) {
+    document.getElementById('menu-icon').className = 'fas fa-bars';
+    document.getElementById('links').className = 'links active';
+  } else {
+    document.getElementById('menu-icon').className = 'fas fa-bars-staggered';
+    document.getElementById('links').className = 'links';
+  }
+}
+
+// Header - Welcome
 const header_welcome_height = document.getElementById('header_welcome').clientHeight;
 
 const scrollFunction = () => {
